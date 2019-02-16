42097ded7c08f318db8d1278240477296e3fa675
Nabarun Gogoi <95nikass@gmail.com>
2019-02-16 18:51:02 +0530

    add tests for user login

--- server/models/user.js
+++ server/models/user.js
@@ -79,7 +79,7 @@ UserSchema.statics.findByCredentials = function(email, password){
         }
         return new Promise((resolve, reject) => {
             bcrypt.compare(password, user.password, (err, res) => {
-                if(user) {
+                if(res) {
                     resolve(user)
                 } else {
                     reject()
